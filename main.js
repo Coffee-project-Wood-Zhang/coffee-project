b4034d006090c1f8fa8b7b0c4c0f5b679918398f
RileyWood9984 <rjw9984@gmail.com>
2023-03-02 10:06:51 -0600

    minor changes

--- main.js
+++ main.js
@@ -9,6 +9,24 @@ var newRoast = document.querySelector('#new-roast-selection')
 var coffeeListTitle = document.querySelector('.coffee-list-title')
 var coffeeList = document.querySelector('#coffees');
 
+// from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
+var coffees = [
+    {id: 1, name: 'Light City', roast: 'light'},
+    {id: 2, name: 'Half City', roast: 'light'},
+    {id: 3, name: 'Cinnamon', roast: 'light'},
+    {id: 4, name: 'City', roast: 'medium'},
+    {id: 5, name: 'American', roast: 'medium'},
+    {id: 6, name: 'Breakfast', roast: 'medium'},
+    {id: 7, name: 'High', roast: 'dark'},
+    {id: 8, name: 'Continental', roast: 'dark'},
+    {id: 9, name: 'New Orleans', roast: 'dark'},
+    {id: 10, name: 'European', roast: 'dark'},
+    {id: 11, name: 'Espresso', roast: 'dark'},
+    {id: 12, name: 'Viennese', roast: 'dark'},
+    {id: 13, name: 'Italian', roast: 'dark'},
+    {id: 14, name: 'French', roast: 'dark'},
+];
+
 function renderCoffee(coffee) {
     var html = '<span class="coffee">';
     // html += '<span>' + coffee.id + '</span>';
@@ -51,23 +69,14 @@ function searchCoffeeFunction() {
     coffeeList.innerHTML = renderCoffees(filteredCoffees);
 }
 
-// from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
-var coffees = [
-    {id: 1, name: 'Light City', roast: 'light'},
-    {id: 2, name: 'Half City', roast: 'light'},
-    {id: 3, name: 'Cinnamon', roast: 'light'},
-    {id: 4, name: 'City', roast: 'medium'},
-    {id: 5, name: 'American', roast: 'medium'},
-    {id: 6, name: 'Breakfast', roast: 'medium'},
-    {id: 7, name: 'High', roast: 'dark'},
-    {id: 8, name: 'Continental', roast: 'dark'},
-    {id: 9, name: 'New Orleans', roast: 'dark'},
-    {id: 10, name: 'European', roast: 'dark'},
-    {id: 11, name: 'Espresso', roast: 'dark'},
-    {id: 12, name: 'Viennese', roast: 'dark'},
-    {id: 13, name: 'Italian', roast: 'dark'},
-    {id: 14, name: 'French', roast: 'dark'},
-];
+
+function reset(e) {
+    e.preventDefault();
+    roastSelection.value = 'all';
+    coffeeListTitle.innerHTML = 'All Coffees!';
+    coffeeList.innerHTML = renderCoffees(coffees);
+    coffeeSearch.value = '';
+}
 
 function freshCoffee(e) {
     e.preventDefault();
@@ -99,5 +108,5 @@ coffeeList.innerHTML = renderCoffees(coffees);
 
 coffeeSearch.addEventListener('keyup', searchCoffeeFunction);
 roastSelection.addEventListener('change', updateCoffees);
-submitButton.addEventListener('click', updateCoffees);
+submitButton.addEventListener('click', reset);
 submitButton2.addEventListener('click', freshCoffee);
